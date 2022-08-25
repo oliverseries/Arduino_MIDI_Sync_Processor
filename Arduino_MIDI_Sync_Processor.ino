69897a4d41ffd59a91fdcd8aad124334bcc0b8df
Oliver-Series <oliver.series1998@gmail.com>
2022-08-25 18:42:52 +1000

    Format Update
    - Renamed some variables
    - Changed the file name
    - Cleaned up formatting

--- Arduino_MIDI_Sync_Processor.ino
+++ Arduino_MIDI_Sync_Processor.ino
@@ -1,42 +1,43 @@
 int i;
 byte commandByte;
-int isrunning;
+int isRunning;
 unsigned long previousMillis = 0;
 
 void setup() {
   i = 0;
-  isrunning = 0;
+  isRunning = 0;
   Serial.begin(31250);
   pinMode(5, OUTPUT);
   pinMode(7, OUTPUT);
 }
 
 void loop() {
+  
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= 4 && isrunning) {
+  if (currentMillis - previousMillis >= 4 && isRunning) {
     if (i%6 == 0) {
       i = 0;
       PORTD = PORTD & B00100000;
     }
   }
-  do{
-    if (Serial.available()){
+  
+  do {
+    if (Serial.available()) {
       commandByte = Serial.read();
       if (commandByte == 0xFA) {
         PORTD = PORTD | B00100000;
-        isrunning = 1;
+        isRunning = 1;
       }
-      if (commandByte == 0xF8 && isrunning) {
+      if (commandByte == 0xF8 && isRunning) {
           PORTD = B10100000;
           previousMillis = millis();
           i += 1;
       }
       if (commandByte == 0xFC) {
         PORTD = B00000000;
-        isrunning = 0;
+        isRunning = 0;
         i = 0;
       }
     }
-  }
-  while (Serial.available() > 2);
+  } while (Serial.available() > 2);
 }
